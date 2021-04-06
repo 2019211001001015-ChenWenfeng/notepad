52608bed8dde235aa38e1d739e76e28728fa3ea7
feng <1376245413@qq.com>
2021-04-06 19:47:58 +0800

    notepad

--- src/main/java/com/notepad/NotepadApplication.java
+++ src/main/java/com/notepad/NotepadApplication.java
@@ -2,12 +2,14 @@ package com.notepad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class NotepadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotepadApplication.class, args);
+        System.out.println("ok");
     }
 
 }
